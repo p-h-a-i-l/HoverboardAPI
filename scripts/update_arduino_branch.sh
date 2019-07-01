5bb82f97ed0150295d4e14bb4fefd558fa80714d
phail <phel@hacknology.de>
2019-07-02 00:03:32 +0200

    Update update_arduino_branch.sh

--- scripts/update_arduino_branch.sh
+++ scripts/update_arduino_branch.sh
@@ -6,7 +6,7 @@ setup_git() {
 }
 
 flatten_submodule() {
-  git checkout -b arduino
+  git checkout -b master_arduino
   
   git rm --cached src/hbprotocol # delete reference to submodule HEAD (no trailing slash)
   git rm .gitmodules             # if you have more than one submodules,
@@ -20,12 +20,12 @@ flatten_submodule() {
   git add .
   git mv src/* ./
 
-  git commit --message "Convert to Arduino Library ($TRAVIS_BUILD_NUMBER)"
+  git commit --message "Auto convert to Arduino Library ($TRAVIS_BUILD_NUMBER)"
 }
 
 upload_files() {
   git remote add upload https://${GITHUB_TOKEN}@github.com/p-h-a-i-l/HoverboardAPI.git > /dev/null 2>&1
-  git push --quiet --set-upstream upload arduino -f
+  git push --quiet --set-upstream upload master_arduino -f
 }
 
 setup_git
