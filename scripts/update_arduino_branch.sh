a0f1408b72dbdde7cbbf34a0d81088bea5121c2c
phail <phel@hacknology.de>
2019-07-01 23:58:48 +0200

    Update update_arduino_branch.sh

--- scripts/update_arduino_branch.sh
+++ scripts/update_arduino_branch.sh
@@ -13,7 +13,10 @@ flatten_submodule() {
                                  # you need to edit this file instead of deleting!
   rm -rf src/hbprotocol/.git     # make sure you have backup!!
   rm -rf src/hbprotocol/examples
+  rm -rf scripts
+  rm -rf .travis.yml
   mv src/hbprotocol/* src/
+  rm -rf src/hbprotocol
   git add .
   git mv src/* ./
 
